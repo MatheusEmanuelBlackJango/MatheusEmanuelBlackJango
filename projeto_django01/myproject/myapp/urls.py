d0aae54c33ccc6ec3b2699651920f8e693b77826
MatheusEmanuelBlackJango <78422930+MatheusEmanuelBlackJango@users.noreply.github.com>
2021-07-06 22:21:40 -0300

    Update urls.py

--- projeto_django01/myproject/myapp/urls.py
+++ projeto_django01/myproject/myapp/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-      path('novo/', views.novo, name='novo'),
-      path('', views.home, name='home')
-] 
+    path('', views.mvp, name='mvp'),
+    path('home/', views.home, name='home')
+]

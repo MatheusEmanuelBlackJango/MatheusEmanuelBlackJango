9ff24d091387f17ef5191659622fb7b3e161d9a4
MatheusEmanuelBlackJango <78422930+MatheusEmanuelBlackJango@users.noreply.github.com>
2021-12-01 20:25:59 -0300

    Update urls.py

--- projeto_django01/myproject/myapp/urls.py
+++ projeto_django01/myproject/myapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mvp, name='mvp'),
-    path('home/', views.home, name='home')
+    path('', views.tabela, name='tabela'),
+    path('mvp/', views.mvp, name='mvp'),
+    path('home/', views.home, name='home'),
+    path('monitoramento_baterias/', views.monitoramento_baterias, name='monitoramento_baterias'),
+    path('monitoramento_trafos/', views.monitoramento_trafos, name='monitoramento_trafos')
+
 ]

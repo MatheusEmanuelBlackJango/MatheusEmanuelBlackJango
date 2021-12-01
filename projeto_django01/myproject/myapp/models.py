b26679a058957e9109b176284503a7c1d73838e9
MatheusEmanuelBlackJango <78422930+MatheusEmanuelBlackJango@users.noreply.github.com>
2021-12-01 20:25:28 -0300

    Update models.py

--- projeto_django01/myproject/myapp/models.py
+++ projeto_django01/myproject/myapp/models.py
@@ -1,6 +1,5 @@
-# Marshmallow example
 import os
-from pprint import pprint
+import pprint
 from dynamorm import DynaModel
 from marshmallow import fields
 
@@ -10,29 +9,34 @@ class Thing(DynaModel):
         resource_kwargs = {
             'region_name': 'us-east-1'
         }
-        name = 'table-teste'.format(env=os.environ.get('ENVIRONMENT', 'dev'))
-        hash_key = 'hash'
-        sort_key = 'sort'
-        read = 10
-        write = 10
+        name = 'tableLambda'.format(env=os.environ.get('ENVIRONMENT', 'dev'))
+        hash_key = 'data'
+        sort_key = 'hora'
+       # read = 10
+       # write = 10
 
     class Schema:
-        key2 = fields.String()
-        key1 = fields.String()
-        key3 = fields.String()
-        key4 = fields.String()
-        hash = fields.String(required=True)
-        sort = fields.String(required=True)
-        coluna = fields.List(cls_or_instance=fields.Float())
+        data = fields.String(required=True)
+        hora = fields.String(required=True)
+        corrente = fields.Number()
+        temperatura = fields.Number()
+        tensao = fields.Number()
 
-    def say_hello(self):
-        print("{coluna}".format(
-            coluna1=self.coluna1
+        #coluna = fields.List(cls_or_instance=fields.Float())
+
+    def ver_dados(self, data=True, hora=True, corrente=True, temperatura=True, tensao=True):
+        print("{data}\n{hora}\n{corrente}\n{temperatura}\n{tensao}".format(
+           data=self.data,
+           hora=self.hora,
+           corrente=self.corrente,
+           temperatura=self.temperatura,
+           tensao=self.tensao
+
         ))
 
 
 if __name__ == '__main__':
-    thing = Thing.get(hash="hash", sort="sort")
+    thing = Thing.get(data="15/11/2021" , hora="15:23")
     if thing:
-        print("\nItens requiridos com sucesso:\n")
-        pprint(thing.say_hello())
+        print("\nDados do Item requiridos com sucesso:\n")
+        print(thing.ver_dados())
